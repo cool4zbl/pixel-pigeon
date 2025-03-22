4bbe78c7388d5fc0f8299433677cb73266dc58a8
binliu <binliu.zhang@gmail.com>
2025-03-22 22:31:27 +0100

    feat: support jumping

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import {Player} from "./Player";
 console.log('Hello World From PixelPigeon!');
 
 const canvasWidth = 1200;
-const canvasHeight = 400;
+const canvasHeight = 300;
 const backgroundColor = '#111';
 const playerWidth = 50;
 const playerHeight = 70;
@@ -32,7 +32,12 @@ const backgroundSpeed = 0.2
 
 const player = new Player(ctx, 30, undefined, playerWidth, playerHeight, playerColor);
 
-let lastTime = 0;
+window.addEventListener('keydown', (e: KeyboardEvent) => {
+    if (e.code === 'Space') {
+        player.jump();
+    }
+})
+
 
 function drawBackground(ctx: CanvasRenderingContext2D, deltaTime: number) {
     backgroundX -= backgroundSpeed * deltaTime;
@@ -45,11 +50,9 @@ function drawBackground(ctx: CanvasRenderingContext2D, deltaTime: number) {
     // draw background image twice to fill canvas
     ctx.drawImage(backgroundImage, backgroundX, 0, canvas.width, canvas.height);
     ctx.drawImage(backgroundImage, backgroundX + canvas.width, 0, canvas.width, canvas.height);
-
 }
 
 function renderGame(ctx: CanvasRenderingContext2D, deltaTime: number) {
-    // ctx!.clearRect(0, 0, canvas.width, canvas.height);
     // set background color
     ctx.fillStyle = backgroundColor;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -66,9 +69,13 @@ function updateGame(deltaTime: number) {
 
 }
 
+let lastTime = 0;
+
 function gameLoop(time: number) {
     const deltaTime = time - lastTime;
     lastTime = time;
+
+    ctx!.clearRect(0, 0, canvas.width, canvas.height);
 
     renderGame(ctx!, deltaTime);
 
