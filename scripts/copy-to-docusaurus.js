4dc753a8e1ed6e5cf0a74b04e4105bbbe2962bf2
binliu <binliu.zhang@gmail.com>
2025-03-22 22:43:29 +0100

    feat: update copy-to-docusaurus.js

--- scripts/copy-to-docusaurus.js
+++ scripts/copy-to-docusaurus.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path'
-import { fileURLToPath } from 'url';
+import {fileURLToPath} from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,7 +13,7 @@ function copyRecursiveSync(src, dest) {
     const stats = fs.statSync(src);
     if (stats.isDirectory()) {
         if (!fs.existsSync(dest)) {
-            fs.mkdirSync(dest, { recursive: true });
+            fs.mkdirSync(dest, {recursive: true});
         }
         fs.readdirSync(src).forEach(child => {
             copyRecursiveSync(path.join(src, child), path.join(dest, child));
@@ -23,12 +23,17 @@ function copyRecursiveSync(src, dest) {
     }
 }
 
-const sourceDir = path.join(__dirname, '..', 'dist');
+const sourceDir = path.join(__dirname, '..', 'dist')
 const destinationDir = path.join(__dirname, '..', '..', 'doodle', 'static', 'pixel-pigeon');
+const assetsDir = path.join(__dirname, '..', 'assets');
+const assertsDestDir = path.join(__dirname, '..', '..', 'doodle', 'static', 'pixel-pigeon', 'assets');
 
 try {
     copyRecursiveSync(sourceDir, destinationDir);
     console.log(`Copied ${sourceDir} to ${destinationDir}`);
+    copyRecursiveSync(assetsDir, assertsDestDir);
+    console.log(`Copied ${assetsDir} to ${assertsDestDir}`);
+
 } catch (err) {
     console.error('Error copying files:', err);
     process.exit(1);
